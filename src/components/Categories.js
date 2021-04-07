9e881e3f62ee33aaaa80908f601375c6bf9241bb
nazorm <nnadoziechinazor@gmail.com>
2021-04-07 01:37:10 +0100

    Categories

---
+++ src/components/Categories.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="categories-container">
+      <ul className="categories-ul">
+        <li className="category">Electronics(12)</li>
+        <li className="category">Face Masks(3)</li>
+        <li className="category">Fresh Food(8)</li>
+        <li className="category">Grocery(6)</li>
+        <li className="category">Home(24)</li>
+        <li className="category">Kids(9)</li>
+      </ul>
+      <p className="all-categories">View all categories</p>
+    </div>
+  );
+};
+
+export default Categories;
