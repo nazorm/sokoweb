15d23abb650fa283475adefd2535903e83c84ebf
nazorm <nnadoziechinazor@gmail.com>
2021-04-07 01:38:02 +0100

    Products

---
+++ src/components/Products.js
@@ -0,0 +1,37 @@
+import chair from "./assets/chair.png"
+
+
+const Products = [
+    {
+        img : chair,
+        name : "kids'2pk Cloth Face Mask",
+        price: "UGX 185,000",
+        id: 1
+    },
+    {
+        img : chair,
+        name : "Norbury Scandinavian Wood Leg Ottoman",
+        price: "UGX 118,400",
+        oldprice : "UGX148,000",
+        id:2
+    },
+    {
+        img : chair,
+        name : "Baby-Cut Carrots - 1lb - Good & Gather",
+        price: "UGX 4,000",
+        id:3
+    },
+    {
+        img : chair,
+        name : "Metal Country Chalet Charm and Wood Bench - Saracina Home",
+        price: "UGX 34,000",
+        id:4
+    },
+    {
+        img : chair,
+        name : "Alternative Protein Ground - 16oz - Good & Gather",
+        price: "UGX 8,000",
+        id:5
+    },
+]
+export default Products
