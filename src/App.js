cc264a4b37f413308728ba7925f08c86e70022dc
nazorm <nnadoziechinazor@gmail.com>
2021-04-09 05:13:57 +0100

    App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 import Home from "./components/Home";
 import shoppingIcon from "./components/assets/cart.svg";
 import homeIcon from "./components/assets/home-icon.svg";
+import whiteCart from "./components/assets/white-cart.svg";
 import userIcon from "./components/assets/user-icon.svg";
+import protection from "./components/assets/protection.svg";
+import fastDelivery from "./components/assets/fast-delivery.svg";
+import customerService from "./components/assets/customer-service.svg";
+import chatIcon from "./components/assets/chat-icon.svg";
 
 function App() {
   return (
@@ -14,7 +19,7 @@ function App() {
           <span className="sokohead-span">
             S
             <img
-              src={shoppingIcon}
+              src={whiteCart}
               className="sokohead-cart"
               alt="sokohead-cart"
             />
@@ -24,13 +29,25 @@ function App() {
       </header>
       <section className="head-nav">
         <div className="intro1">
-          <img src={homeIcon} alt="introhome-icon" className="introhome-icon" />
-          <h3 className="head-target">Target</h3>
-          <p className="head-address">Charm Towers,Plot 12 Nkrumah Rd, Kampala, Ug</p>
+          <div className="intro-home">
+            <img
+              src={homeIcon}
+              alt="introhome-icon"
+              className="introhome-icon"
+            />
+          </div>
+
+          <p className="head-target">
+            Target
+            <br />
+            <span className="head-address">
+              Cham Towers,Plot 12 Nkrumah Rd, Kampala, Ug
+            </span>
+          </p>
         </div>
-        <nav className="intro-nav">
-          <ul>
-            <li className="nav-item">
+        <nav className="headintro-nav">
+          <ul className="intro-nav">
+            <li className="intronav-item">
               <img
                 src={shoppingIcon}
                 alt="nav-icon"
@@ -38,36 +55,47 @@ function App() {
               />
               Bag
             </li>
-            <li className="nav-item">
+            <li className="intronav-item">
               <img src={userIcon} alt="nav-icon" className="nav-usericon" />
               Account
             </li>
           </ul>
         </nav>
       </section>
+      <span className="line"></span>
       <Home />
       <section className="features">
         <div className="feature">
-          <img src="" className="feature-image" alt="feature-image" />
+          <img
+            src={fastDelivery}
+            className="feature-image"
+            alt="feature-image"
+          />
           <p className="feature-text">Fast Delivery</p>
         </div>
         <div className="feature">
-          <img src="" className="feature-image" alt="feature-image" />
+          <img src={protection} className="feature-image" alt="feature-image" />
           <p className="feature-text">Buyer Protection</p>
         </div>
         <div className="feature">
-          <img src="" className="feature-image" alt="feature-image" />
+          <img
+            src={customerService}
+            className="feature-image"
+            alt="feature-image"
+          />
           <p className="feature-text">Customer Support</p>
         </div>
       </section>
       <footer className="footer">
         <p className="store-details">STORE DETAILS</p>
-      <h3 className="footer-target">Target</h3>
-          <p className="footer-address">Charm Towers,Plot 12 Nkrumah Rd, Kampala, Ug</p>
-          <button>
-          <img src="" className="footerwhatsapp-icon" alt="whatsapp-icon" />
+        <h3 className="footer-target">Target</h3>
+        <p className="footer-address">
+          Cham Towers,Plot 12 Nkrumah Rd, Kampala, Ug
+        </p>
+        <button className="chat-btn">
+          <img src={chatIcon} className="chat-icon" alt="chat" />
           Chat with us
-          </button>
+        </button>
       </footer>
     </div>
   );
