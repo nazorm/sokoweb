d3fd3866f7a225c6c9e128f4c0920bd1ab491b1f
nazorm <nnadoziechinazor@gmail.com>
2021-04-09 05:14:07 +0100

    Shopping Bag

--- src/components/Bag.js
+++ src/components/Bag.js
@@ -1,16 +1,23 @@
 import React from "react";
+import sadFace from "./assets/sad-face.svg";
 
 const Bag = () => {
   return (
-    <div>
+    <div className="bag-container">
       <header className="bag-header">
         <h3 className="bagheader-name">Bag</h3>
         <h3 className="bag-count">0</h3>
       </header>
       <section className="emptycart">
-        <img src="" className="emptycart-icon" alt="emptycart-icon" />
+        <img
+          src={sadFace}
+          className="emptycart-icon"
+          alt="emptycart-icon"
+          height="100px"
+        />
         <p className="emptycart-text">
           It's empty here
+          <br />
           <span className="start-shopping">
             Start Shopping to add items to your bag
           </span>
