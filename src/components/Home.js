b871a0a9073b9bfe0ae81877c5a84f9254341776
nazorm <nnadoziechinazor@gmail.com>
2021-04-09 05:14:56 +0100

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,27 @@ import React from "react";
 import "../App.css";
 import { Input } from "antd";
 import ElectronicsPage from "./ElectronicsPage";
+import Categories from "./Categories";
+import Bag from "./Bag";
 const { Search } = Input;
 
-
-const Home=() =>{
-    const onSearch=()=>{
-        alert("here")
-    }
+const Home = () => {
+  const onSearch = () => {
+    alert("here");
+  };
   return (
     <div className="home-container">
-      <Search placeholder="Search for products" onSearch={onSearch} enterButton />
-      <ElectronicsPage/>
+      <Search
+        className="search-bar"
+        placeholder="Search for products"
+        onSearch={onSearch}
+        enterButton
+      />
+      <Categories />
+      <ElectronicsPage />
+      <Bag />
     </div>
   );
-}
+};
 
 export default Home;
