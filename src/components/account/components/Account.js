4804d58526188b7a515684bdf54ac63d1c15e03e
nazorm <nnadoziechinazor@gmail.com>
2021-04-14 03:12:50 +0100

    Address Screen

--- src/components/account/components/Account.js
+++ src/components/account/components/Account.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBorderAll } from "react-icons/fa";
 import { GrLocation, GrTarget } from "react-icons/gr";
 import { BsFilter } from "react-icons/bs";
 import homeIcon from "../../assets/home-icon.svg";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Account = () => {
+  const [displayOrder, setDisplayOrder] = useState(true);
+  const [displayAddress, setDisplayAddress] = useState(false);
+  const goToOrder = () => {
+    setDisplayOrder(true);
+    setDisplayAddress(false);
+  };
+  const goToAddress = () => {
+    setDisplayOrder(false);
+    setDisplayAddress(true);
+  };
+
   return (
     <section className="account-section">
       <div className="account-header">
@@ -16,17 +28,19 @@ const Account = () => {
       <section className="account-info">
         <nav className="order-nav">
           <ul>
-            <li>
+            <li onClick={goToOrder}>
               <FaBorderAll className="account-icon" />
               <span>My Order</span>
             </li>
-            <li>
+            <li onClick={goToAddress}>
               <GrLocation className="account-icon" />
               <span>My Addresses</span>
             </li>
           </ul>
         </nav>
-        <div className="all-orders">
+        <div
+          className={displayOrder ? "allorders-display" : "noallorders-display"}
+        >
           <div className="allorders-heading">
             <h3>Showing all orders</h3>
             <button className="filter-btn">
@@ -83,6 +97,26 @@ const Account = () => {
               <div className="status">
                 <div className="deliveredstatus-signifier"></div>
                 <p className="order-delivered">Delivered</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className={displayAddress ? "display-address" : "nodisplay-address"}
+        >
+          <div className="addresses">
+            <div className="address-card">
+              <h3 className="customer-name">Shrey Karah</h3>
+              <p className="customer-address">
+                Quench Ville, Plot 12 Nkrumah Rd Kampala, Uganda
+              </p>
+              <p className="customer-phone">+256 76 633 4574</p>
+              <span className="edit">Edit</span>
+              <span className="delete">Delete</span>
+            </div>
+            <div className="address-card add-address-card">
+              <div className="add-address">
+                <Link to="/">+ Add New Address</Link>
               </div>
             </div>
           </div>
